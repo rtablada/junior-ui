d430d949e2ee325062f578b1dd721c566a709288
Peter Court <peter@petercourt.com>
2016-08-28 21:11:21 -0500

    added sub nav menu and toggle

--- app/application/controller.js
+++ app/application/controller.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
+  actions: {
+    toggleNav() {
+      const ham = document.querySelector(".sub__nav");
+      ham.classList.toggle('sub__nav--off')
+    },
+  },
 });
