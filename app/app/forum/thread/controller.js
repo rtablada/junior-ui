f7971c40d3a4b43cf3f1800f09145f3ec5568223
Peter Court <peter@petercourt.com>
2016-08-29 17:07:23 -0500

    working on auth login post and delete

--- app/app/forum/thread/controller.js
+++ app/app/forum/thread/controller.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service(),
   actions: {
-    addComment(post, {markdown}, reset) {
+    addComment(post, {
+      markdown
+    }, reset) {
       const comment = this.store.createRecord('comment', {
         post,
         markdown,
@@ -16,8 +19,9 @@ export default Ember.Controller.extend({
     },
     deletePost(post) {
       post.destroyRecord().then(() => {
-
+        this.transitionToRoute('app.forum.index');
       });
     },
+
   },
 });
