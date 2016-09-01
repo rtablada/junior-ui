f2bc9922eae2197525dbafc5e02ea821b9c76c4b
Peter Court <peter@petercourt.com>
2016-09-01 16:26:02 -0500

    added sorting

--- app/comment/model.js
+++ app/comment/model.js
@@ -3,5 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   post: DS.belongsTo('post'),
   user: DS.belongsTo('user'),
-  markdown: DS.attr('string')
+  markdown: DS.attr('string'),
+  createdAt: DS.attr(),
+  createdAtFormatted: fromNow(momentComputed('createdAt')),
 });
