d7c7b7e851fd664c6aedeb699c07d1c73f96edce
Peter Court <peter@petercourt.com>
2016-09-02 13:25:15 -0500

    fixed delete not showing up

--- app/models/post.js
+++ app/models/post.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 import fromNow from 'ember-moment/computeds/from-now';
-import momentComputed from 'ember-moment/computeds/moment';
+import moment from 'moment';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   postType: DS.attr('string'),
@@ -9,6 +10,8 @@ export default DS.Model.extend({
   comments: DS.hasMany('comment'),
   user: DS.belongsTo('user'),
   createdAt: DS.attr(),
-  createdAtFormatted: fromNow(momentComputed('createdAt')),
+  createdAtFormatted: fromNow(Ember.computed('createdAt', function () {
+    return moment.utc(this.get('createdAt'));
+  })),
   tags: DS.hasMany('tag'),
 });
